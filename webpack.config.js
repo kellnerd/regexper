e64cde51053f19bb213b8d7b8e493b4d80ed79a2
David Kellner <52860029+kellnerd@users.noreply.github.com>
2022-01-13 20:48:13 +0100

    Run webpack in "production" mode by default

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
+  mode: process.env.NODE_ENV || 'production',
   devtool: 'source-map',
   entry: './src/js/main.js',
   output: {
